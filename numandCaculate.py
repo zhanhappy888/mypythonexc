85dd4177d51480bf5b193941fd3387579c825f75
zhanhappy <zhanjiang@ruc.edu.cn>
2017-05-12 16:02:42 +0800

    add 3 python files

---
+++ numandCaculate.py
@@ -0,0 +1,23 @@
+print "Hens", 25 + 30 / 6 
+print "Roosters", 100 - 25 * 3 % 4 
+
+print 1/4
+
+print 1.0/4
+
+print 4 % 2
+ 
+print "Now I will count the eggs:" 
+ 
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 
+ 
+print "Is it true that 3 + 2 < 5 - 7?" 
+
+print 3 + 2 < 5 - 7 
+ 
+print "What is 3 + 2?", 3 + 2 
+print "What is 5 - 7?", 5 - 7 
+
+print "Is it greater?", 5 > -2 
+print "Is it greater or equal?", 5 >= -2 
+print "Is it less or equal?", 5 <= -2 
---
+++ printstring.py
@@ -0,0 +1,18 @@
+#如果你使用了非 ASCII 字符而且碰到了编码错误，记得在最顶端加一 行 # -- coding: utf-8 -- 
+
+print "Hello World!" 
+print "Hello Again" 
+print "I like typing this." 
+print "This is fun." 
+print 'Yay! Printing.'    # and the comment after is ignored 
+ 
+# You can also use a comment to "disable" or comment out a piece of code: 
+# print "This won't run." 
+ 
+print "This will run." 
+
+
+print "I'd much rather you 'not'." 
+print 'I "said" do not touch this.' 
+print ''' this's is "a book"!!! '''
+print "显示汉字"
